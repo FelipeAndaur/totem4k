4fdbe66eb25ccf3c54431bd92723baccd5a59384
Felipe Vargas Andaur <felipe@appy.cl>
2024-07-11 01:38:26 +0200

    Q1HD

--- lib/src/question_page.dart
+++ lib/src/question_page.dart
@@ -15,7 +15,7 @@ class _QuestionPageState extends State<QuestionPage> {
   List<Map<String, String>> questions = [
     {
       "image":
-          "assets/images/Q1-Frutas.png" // Primera pregunta con la imagen incluida
+          "assets/images/Q1-FrutasHD.png" // Primera pregunta con la imagen incluida
     },
     // Agrega más preguntas e imágenes aquí
   ];
